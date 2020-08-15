66b9fd692082aec200beea3a36cdaafb5df0ea60
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-14 23:31:09 -0500

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,44 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import Header from '../components/header';
-import Layout from '../components/layout';
 import Container from '../components/container';
-import { CLIENT_NAME } from '../lib/constants';
-// import SearchBox from '../components/search-box';
-import SocialMediaIcons from '../components/social-media-icons';
-import Card from '../components/card';
+import MoreCards from '../components/more-cards';
+// import HeroPost from '../components/hero-post';
+import Intro from '../components/intro';
+import Layout from '../components/layout';
+import { getAllPosts } from '../lib/api';
+import Head from 'next/head';
+import { CMS_NAME } from '../lib/constants';
+import Post from '../types/post';
+import Header from '../components/header';
 
-interface Props {
+type Props = {
+	allPosts: Post[];
 	props: string | number;
-	preview?: boolean;
-}
+};
 
-const Index = ({ preview, props }: Props) => {
+const Index = ({ allPosts, props }: Props) => {
+	// const heroPost = allPosts[0];
+	const morePosts = allPosts.slice(0);
 	return (
 		<>
 			<Header props={props} />
-			<Layout preview={preview}>
+			<Layout>
 				<Head>
-					<title>{`${CLIENT_NAME} landing page`}</title>
+					<title>Next.js Blog Example with {CMS_NAME}</title>
 				</Head>
 				<Container>
-					<h2 className='md:text-5xl font-bold text-center font-serif tracking-tight md:tracking-tighter leading-tight mb-8 mt-2'>
-						<Link href='/'>
-							<a className='hover:text-cimaRed cursor-text select-text text-black px-8'>
-								Thanks for Dropping by
-							</a>
-						</Link>
-					</h2>
-					{/* <SearchBox /> */}
-
+					<Intro />
+					{/* {heroPost && (
+						<HeroPost
+							title={heroPost.title}
+							coverImage={heroPost.coverImage}
+							date={heroPost.date}
+							author={heroPost.author}
+							slug={heroPost.slug}
+							excerpt={heroPost.excerpt}
+						/>
+					)} */}
+					<div className='max-w-5xl mt-5 mb-5 grid mx-auto content-center justify-center items-center text-center'>
+						{morePosts.length > 0 && <MoreCards posts={morePosts} />}
+					</div>
 				</Container>
 			</Layout>
 		</>
@@ -38,3 +46,18 @@ const Index = ({ preview, props }: Props) => {
 };
 
 export default Index;
+
+export const getStaticProps = async () => {
+	const allPosts = getAllPosts([
+		'title',
+		'date',
+		'slug',
+		'author',
+		'coverImage',
+		'excerpt'
+	]);
+
+	return {
+		props: { allPosts }
+	};
+};
---
+++ pages/posts/[slug].tsx
@@ -0,0 +1,104 @@
+import { useRouter } from 'next/router';
+import ErrorPage from 'next/error';
+import Container from '../../components/container';
+import PostBody from '../../components/post-body';
+import Header from '../../components/header';
+import PostHeader from '../../components/post-header';
+import Layout from '../../components/layout';
+import { getPostBySlug, getAllPosts } from '../../lib/api';
+import PostTitle from '../../components/post-title';
+import Head from 'next/head';
+import { CMS_NAME } from '../../lib/constants';
+import markdownToHtml from '../../lib/markdownToHtml';
+import PostType from '../../types/post';
+import { Fragment } from 'react';
+
+type SlugProps = {
+	post: PostType;
+	morePosts: PostType[];
+	preview?: boolean;
+	props: string | number;
+};
+
+const Post = ({ post, morePosts, preview, props }: SlugProps) => {
+	const router = useRouter();
+	if (!router.isFallback && !post?.slug) {
+		return <ErrorPage statusCode={404} />;
+	}
+	return (
+		<Fragment>
+			<Header props={props} />
+			<Layout preview={preview}>
+				<Container>
+					{router.isFallback ? (
+						<PostTitle>Loading…</PostTitle>
+					) : (
+						<>
+							<article className='mb-32'>
+								<Head>
+									<title>
+										{post.title} | Next.js Blog Example with {CMS_NAME}
+									</title>
+									<meta property='og:image' content={post.ogImage.url} />
+								</Head>
+								<PostHeader
+									title={post.title}
+									coverImage={post.coverImage}
+									date={post.date}
+									author={post.author}
+									social={post.social}
+								/>
+								<PostBody content={post.content} />
+							</article>
+						</>
+					)}
+				</Container>
+			</Layout>
+		</Fragment>
+	);
+};
+
+export default Post;
+
+type Params = {
+	params: {
+		slug: string;
+	};
+};
+
+export async function getStaticProps({ params }: Params) {
+	const post = getPostBySlug(params.slug, [
+		'title',
+		'date',
+		'slug',
+		'author',
+		'content',
+		'ogImage',
+		'coverImage'
+	]);
+	const content = await markdownToHtml(post.content || '');
+
+	return {
+		props: {
+			post: {
+				...post,
+				content
+			}
+		}
+	};
+}
+
+export async function getStaticPaths() {
+	const posts = getAllPosts(['slug']);
+
+	return {
+		paths: posts.map(posts => {
+			return {
+				params: {
+					slug: posts.slug
+				}
+			};
+		}),
+		fallback: false
+	};
+}
