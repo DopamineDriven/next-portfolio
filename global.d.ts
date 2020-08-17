950545229a48bdad941f83b3a03d78f9c7bc3a2f
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-16 23:38:10 -0500

    updates

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,19 @@
-declare module "remark-html" {
+declare module 'remark-html' {
 	const html: any;
 	export default html;
 }
+
+import {
+	IconDefinition,
+	IconPrefix,
+	IconName
+} from '@fortawesome/fontawesome-common-types';
+export const definition: IconDefinition;
+export const faWindowClose: IconDefinition;
+export const prefix: IconPrefix;
+export const iconName: IconName;
+export const width: number;
+export const height: number;
+export const ligatures: string[];
+export const unicode: string;
+export const svgPathData: string;
