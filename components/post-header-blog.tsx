21b08b1dc4351a422ddc636a4e5df0abcdf3378e
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-24 03:08:51 -0500

    code highlighting success, prose success

--- components/post-header-blog.tsx
+++ components/post-header-blog.tsx
@@ -13,23 +13,23 @@ type Props = {
 
 const PostHeaderBlog = ({ title, coverImage, date, author }: Props) => {
 	return (
-		<>
+		<div className='mt-4'>
 			<PostTitle>{title}</PostTitle>
-			<div className='hidden md:block md:mb-12'>
+			<div className='hidden md:flex md:content-center md:mb-12 md:mx-auto md:justify-center md:mt-4'>
 				<Avatar name={author.name} picture={author.avatar} />
 			</div>
-			<div className='mb-8 md:mb-16 -mx-5 sm:mx-0'>
+			<div className='mb-8 md:mb-8 -mx-5 sm:mx-0 md:flex md:mx-auto justify-center md:text-center'>
 				<CoverImage title={title} src={coverImage} />
 			</div>
-			<div className='max-w-2xl mx-auto'>
+			<div className='max-w-2xl mx-auto md:flex md:justify-center'>
 				<div className='block md:hidden mb-6'>
 					<Avatar name={author.name} picture={author.avatar} />
 				</div>
-				<div className='mb-6 text-lg'>
+				<div className='mb-2 text-xl'>
 					<DateFormater dateString={date} />
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
