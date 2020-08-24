291165650bb195c09d5fcff76f2d3d2d707c5b50
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-08-23 21:47:12 -0500

    siteconfig+sitemetadata for SEO

---
+++ site.config.js
@@ -0,0 +1,16 @@
+export default {
+    siteMetadata: {
+      title: "next-portfolio",
+      author: {
+        name: "Andrew Ross",
+        summary: "Next+TS+Tailwindcss ft SSG",
+      },
+      description: "A portfolio created with Next.js and Tailwind.css",
+      siteUrl: "https://next-portfolio-asross.vercel.app/",
+      language: "en-US",
+      social: {
+        twitter: "Dopamine_Driven",
+      },
+    },
+  };
+  
